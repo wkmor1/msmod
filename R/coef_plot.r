ead0713b7f20847ae7e0f119a2e2cf59eace4d89
wkmor1 <wkmor1@gmail.com>
2015-06-18 10:10:45 +1000

    function to plot coefficients of an msm

---
+++ R/coef_plot.r
@@ -0,0 +1,52 @@
+#' Plot the coefficients of a multispecies model
+#'
+#' @param x An object of class 'glmerMod'
+#' @examples
+#'
+#' library(msm)
+#' data(eucs)
+#' msm_fit <- msm('present', 'plot', 'logit_rock', 'species', 'ln_sla', eucs)
+#' coef_plot(msm_fit) 
+#'
+#' @export
+
+coef_plot <- function(x) {
+
+  x %>%
+  is_glmerMod(.)
+
+ "forceSymmetric" %>%
+    assign(Matrix::forceSymmetric)
+
+  dplyr::data_frame(
+    coef=
+      x %>%
+      methods::slot('pp') %>%
+      base::get('X', envir=.) %>%
+      base::colnames(.),
+    `Coefficient value`=
+      x %>%
+      lme4::fixef(.),
+    ci=
+      x %>%
+      lme4::vcov.merMod(.) %>%
+      methods::slot('factors') %>%
+      base::getElement('correlation') %>%
+      methods::slot('sd') %>%
+      magrittr::multiply_by(1.96)
+  ) %>%
+
+  ggplot2::ggplot(
+    ggplot2::aes(x=`Coefficient value`, y=coef)
+  ) + 
+  ggplot2::geom_point() +
+  ggplot2::geom_errorbarh(
+    ggplot2::aes(
+      xmin=`Coefficient value` - ci,
+      xmax=`Coefficient value` + ci,
+      height=0)  
+  ) +
+  ggplot2::scale_y_discrete(name='')
+}
+
+utils::globalVariables(c(".", "Coefficient value", "ci"))
