a814fbf8c1fe310749637b3bbed3b805a12c6036
wkmor1 <wkmor1@gmail.com>
2015-06-20 18:46:18 +1000

    new util functions to help using magrittr in msm

--- R/utils.r
+++ R/utils.r
@@ -2,7 +2,9 @@
 #' @import R2jags
 #' @importFrom magrittr %>%
 #' @importFrom magrittr %<>%
-#' @importFrom magrittr %T>%
+
+# to use magrittr shortcut
+utils::globalVariables(".")
 
 # Check if a glmerMod class
 is_glmerMod <- function(x) {
@@ -12,19 +14,43 @@ is_glmerMod <- function(x) {
   magrittr::not(.) %>%
   {
     if (.) {
-      'x must object of class "glmerMod"' %>%
-     base::stop(call.=FALSE)
+      "x must object of class 'glmerMod'" %>%
+     base::stop(call. = FALSE)
     }
   }
 }
 
-# magrittr like functions to return something else if condition is met or not
-return_if <- function(x, test, y) {
+# magrittr like functions to return something else if condition is not met
+return_if_not <- function(x, test, y) {
   if (test) y else x
 }
 
-return_if_not <- function(x, test, y) {
-  if (test) x else y
+# bind y to x if y not in z
+bind_if_not_in <- function(x, y, z, out=base::get(z, parent.frame())) {
+  x %>%
+  return_if_not(
+    y %>%
+    magrittr::extract2(z) %>%
+    base::is.null(.),
+    x %>%
+    magrittr::inset2(z, out)
+  )
 }
 
-
+# evaluate a function with a list of named arguments (do.call doesnt keep 
+# the names.)
+eval_with_args <- function(args, fun) {
+  fun <-
+    base::substitute(fun) %>%
+    base::deparse(.)
+  
+  base::names(args) %>%
+  base::paste0(., "=") %>%
+  base::paste0(names(args), collapse = ",") %>%
+  base::sprintf(
+    "with(args, %s(%s))",
+    fun,
+    .) %>%
+  base::parse(text = .) %>%
+  base::eval(.)
+}
