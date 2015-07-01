1c7ed29fe4068eb8eb59cd92aee1c116077d5113
wkmor1 <wkmor1@gmail.com>
2015-07-01 16:59:59 +1000

    jags model now includes traits

--- R/msm.r
+++ R/msm.r
@@ -17,8 +17,8 @@
 #'   method='jags')
 #' @export
 msm <- function(y, sites, x, species, traits, data, site_re=FALSE,
-                method=c("glmer", "jags"), ...) {
-
+  method=c("glmer", "jags"), ...)
+{
   x %<>% dplyr::select_vars_(base::names(data), .)
 
   traits %<>% dplyr::select_vars_(base::names(data), .)
@@ -39,12 +39,11 @@ msm <- function(y, sites, x, species, traits, data, site_re=FALSE,
     dplyr::distinct(.) %>%
     base::nrow(.)
 
-  dots <- list(...)
+  dots <-
+    list(...)
 
-  method <- base::match.arg(method)
-
-  method %>%
-  switch(
+  base::match.arg(method) %>%
+  base::switch(
     glmer =
       msm_glmer(y, sites, x, species, n_species, traits, data, site_re, dots),
     jags =
@@ -53,19 +52,19 @@ msm <- function(y, sites, x, species, traits, data, site_re=FALSE,
 }
 
 msm_glmer <- function(y, sites, x, species, n_species, traits, data, site_re,
-  dots) {
-  
+  dots)
+{
   ' %s ~ %s + %s + (1 + %s | %s)' %>%
   base::paste0(
-    site_re %>% 
-    dplyr::first(.) %>% 
+    site_re %>%
+    dplyr::first(.) %>%
     base::ifelse(' + (1 | %s)', '')
   ) %>%
   base::sprintf(
     y,
-    x %>% 
+    x %>%
     base::paste(collapse = ' + '),
-    x %>% 
+    x %>%
     base::expand.grid(traits) %>%
     base::do.call(
       function(...) base::paste(..., sep = ':'), .
@@ -86,84 +85,173 @@ msm_glmer <- function(y, sites, x, species, n_species, traits, data, site_re,
 }
 
 msm_jags <- function(y, sites, x, species, n_species, traits, data, site_re,
-  dots) {
-
+  dots)
+{
   model <-
     function() {
       for (site in 1:n_sites) {
-        Z[site, 1:n_species] ~ dmnorm(Mu[site, ], Tau[, ])
+        Z[site, 1:n_species] ~ dmnorm(Mu[site, ], Tau)
         for (species in 1:n_species) {
-          Mu[site, species] <-
-            inprod(Beta_raw[species, ], X[site, ])
-          Y[site, species] ~ dbern(P[site, species])
-          P[site, species] <- step(Z[site, species])
+          Mu[site, species] <- inprod(Beta_raw[species, ], X_site[site, ])
+          Y[site, species] ~ dbern(step(Z[site, species]))
         }
       }
       for (species in 1:n_species) {
+        sigma_[species] <- sqrt(Sigma[species, species])
+        B_raw[species, 1:K] ~ dmnorm(B_hat_raw[species, ], Tau_B)
         for (k in 1:K) {
-          Beta_raw[species, k] ~ dnorm(mu_raw[k], tau[k])
-          Beta[species, k] <- Beta_raw[species, k] /
-            sqrt(Sigma[species, species])
+          B_hat_raw[species, k] <- sum(B_hat_raw_[species, k, ])
+          Beta_raw[species, k] <- B_raw[species, k] * xi[k]
+          Beta[species, k] <- Beta_raw[species, k] / sigma_[species]
+          for (j in 1:J) {
+            B_hat_raw_[species, k, j] <- G_raw[k, j] * X_sp[species, j]
+            Gamma_hat[species, k, j] <- B_hat_raw_[species, k, j] * xi[k] /
+              sigma_[species] / X_sp[species, j]
+          }
         }
         for (species_ in 1:n_species) {
-          Rho[species, species_] <- Sigma[species, species_] /
-            (sqrt(Sigma[species, species]) * sqrt(Sigma[species_, species_]))
+          Rho[species, species_] <- Sigma[species, species_] / sigma_[species] *
+            sigma_[species_]
         }
       }
       for (k in 1:K) {
-        mu_raw[k] ~ dnorm(0, .0001)
-        tau[k] <- pow(sigma_raw[k], -2)
-        sigma_raw[k] ~ dunif(0, 100)
+        xi[k] ~ dunif(0, 100)
+        sigma[k] <- sqrt(Sigma_B[k, k]) * xi[k]
+        for (k_ in 1:K) {
+          Rho_B[k, k_] <- Sigma_B[k, k_] /
+            (sqrt(Sigma_B[k, k]) * sqrt(Sigma_B[k_, k_]))
+        }
+        for (j in 1:J) {
+          G_raw_[k, j] ~ dnorm(0, .0001)
+          G_raw[k, j] <- ifelse(k == 1 && j > 1, 0, G_raw_[k, j])
+          Gamma[k, j] <- mean(Gamma_hat[, k, j])
+        }
       }
-      Tau[1:n_species, 1:n_species] ~ dwish(I[, ], df)
+      Tau ~ dwish(I, df)
       Sigma <- inverse(Tau)
+      Tau_B ~ dwish(I_B, df_B)
+      Sigma_B <- inverse(Tau_B)
     }
-  
+
   Y <-
     y %>%
     dplyr::select_(data, .) %>%
     base::unlist(.) %>%
     base::matrix(ncol = n_species)
 
-  X =
+  K <-
+    x %>%
+    base::length(.) %>%
+    magrittr::add(1)
+
+  X_site <-
     data %>%
     dplyr::distinct_(sites) %>%
     dplyr::select_(x) %>%
     magrittr::inset2('(Intercept)', value = 1) %>%
-    base::rev(.)
-    
+    base::subset(
+      select =
+        K %>%
+        base::c(
+          {.} %>%
+          magrittr::subtract(1) %>%
+          base::seq_len(.)
+        )
+    )
+
+  J <-
+    traits %>%
+    base::length(.) %>%
+    magrittr::add(1)
+
+  X_sp <-
+    data %>%
+    dplyr::distinct_(species) %>%
+    dplyr::select_(traits) %>%
+    magrittr::inset2('(Intercept)', value = 1) %>%
+    base::subset(
+      select =
+        J %>%
+        base::c(
+          {.} %>%
+          magrittr::subtract(1) %>%
+          base::seq_len(.)
+        )
+    )
+
   inits <-
-    base::list(
-      Tau =
-        n_species %>%
-        base::diag(.)
+    dots %>%
+    base::getElement('n.chains') %>%
+    base::is.null(.) %>%
+    base::ifelse(
+      yes = 3,
+      no =
+        dots %>%
+        base::getElement('n.chains')
     ) %>%
-    magrittr::inset2(
-      'Z',
-       Y %>%
-       magrittr::subtract(.5)
-    ) %>%
-    base::list(.) %>%
-    base::rep(3)
+    base::seq_len(.) %>%
+    base::lapply(
+      function(x)
+      {
+        xi <-
+          K %>%
+          stats::runif(.)
+
+        B_raw <-
+          n_species %>%
+          magrittr::multiply_by(K) %>%
+          stats::runif(-1, 1) %>%
+          matrix(K, n_species) %>%
+          magrittr::divide_by(xi) %>%
+          base::t(.)
+
+        base::list(
+          Z =
+            Y %>%
+            magrittr::subtract(.5),
+          Tau =
+            n_species %>%
+            base::diag(.),
+          B_raw = B_raw,
+          xi = xi,
+          Tau_B =
+            K %>%
+            base::diag(.),
+          G_raw_ =
+            J %>%
+            magrittr::multiply_by(K) %>%
+            stats::runif(-1, 1) %>%
+            matrix(K, J)
+        )
+      }
+    )
 
   parameters.to.save <-
-    c('Beta', 'Rho', 'Mu')
-  
+    c('Beta', 'Gamma', 'Rho_B', 'sigma', 'Rho', 'Gamma_hat')
+
   base::list(
     Y = Y,
-    X = X,
-    K =
-      X %>%
-      base::ncol(.),
+    X_site = X_site,
+    X_sp = X_sp,
+    K = K,
+    J = J,
     n_species = n_species,
     n_sites =
-      X %>%
-      base::nrow(.),
+      X_site %>%
+      base::NROW(.),
     I =
       n_species %>%
       base::diag(.),
     df =
       n_species %>%
+      magrittr::add(1),
+    I_B =
+      X_sp %>%
+      base::NCOL(.) %>%
+      base::diag(.),
+    df_B =
+      X_sp %>%
+      base::NCOL(.) %>%
       magrittr::add(1)
   ) %>%
   base::list(model) %>%
@@ -171,9 +259,11 @@ msm_jags <- function(y, sites, x, species, n_species, traits, data, site_re,
   bind_if_not_in(dots, 'inits') %>%
   bind_if_not_in(dots, 'parameters.to.save') %>%
   bind_if_not_in(dots, 'n.iter', 200) %>%
+  bind_if_not_in(dots, 'DIC', FALSE) %>%
   base::c(dots) %>%
   eval_with_args(R2jags::jags)
 }
 
-c("n_sites", "inprod", "Beta_raw", "Z", "K", "pow", "sigma_raw") %>%
-  utils::globalVariables(.)
+c("n_sites", "inprod", "Z", "pow", "inverse",
+  "Tau", "B_raw", "G_raw_", "xi", "Tau_B") %>%
+utils::globalVariables(.)
