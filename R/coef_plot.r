e1d9babd6d1ce52751fabbd67ea99b133e619646
wkmor1 <wkmor1@gmail.com>
2015-11-30 05:56:08 +0000

    rjags method for coef_plot

--- R/coef_plot.r
+++ R/coef_plot.r
@@ -1,22 +1,40 @@
 #' Plot the coefficients of a multispecies model
 #'
-#' @param x An object of class 'glmerMod'
+#' @param x An object of class "glmerMod", "rjags" or "rjags_parallel"
 #' @examples
-#' msm_fit <- msm('present', 'plot', 'logit_rock', 'species', 'ln_sla', eucs)
-#' coef_plot(msm_fit)
+#' msm_glmer <- msm('present', 'plot', 'logit_rock', 'species', 'ln_sla', eucs)
+#' coef_plot(msm_glmer)
+#' msm_jags <- msm('present', 'plot', 'logit_rock', 'species', data = eucs, type = 'jsdm',
+#'   method = 'jags')
+#' coef_plot(msm_jags)
 #' @export
 
 setGeneric(
   "coef_plot",
   function(x) {
-    standardGeneric("coef_plot")
+    base::standardGeneric("coef_plot")
   }
 )
+
+coef_plot_ <- function(coefs) {
+  ggplot2::ggplot(
+    coefs,
+    ggplot2::aes(x = `Coefficient value`, y = coef)
+  ) +
+    ggplot2::geom_point() +
+    ggplot2::geom_errorbarh(
+      ggplot2::aes(
+        xmin = `Coefficient value` - ci,
+        xmax = `Coefficient value` + ci,
+        height = 0)
+    ) +
+    ggplot2::scale_y_discrete(name = "")
+}
 
 #' @describeIn coef_plot coefficient plot for glmer model
 setMethod(
   "coef_plot",
-  c(x = "glmerMod"),
+  base::c(x = "glmerMod"),
   function(x) {
     dplyr::data_frame(
       coef =
@@ -24,7 +42,7 @@ setMethod(
         methods::slot("pp") %>%
         base::get("X", envir = .) %>%
         base::colnames(.),
-      `Coefficient value` =
+      "Coefficient value" =
         x %>%
         lme4::fixef(.),
       ci =
@@ -35,19 +53,43 @@ setMethod(
         methods::slot("sd") %>%
         magrittr::multiply_by(1.96)
     ) %>%
-
-    ggplot2::ggplot(
-      ggplot2::aes(x = `Coefficient value`, y = coef)
-    ) +
-    ggplot2::geom_point() +
-    ggplot2::geom_errorbarh(
-      ggplot2::aes(
-        xmin = `Coefficient value` - ci,
-        xmax = `Coefficient value` + ci,
-        height = 0)
-    ) +
-    ggplot2::scale_y_discrete(name = "")
+    coef_plot_
   }
 )
 
-utils::globalVariables(c("Coefficient value", "ci"))
+coef_plot_rjags <- function(x) {
+  samples <-
+    magrittr::extract2(x, "BUGSoutput") %>%
+    magrittr::extract2("sims.list") %>%
+    magrittr::extract2("B") %>% 
+    base::apply(base::c(1, 3), base::mean)
+  dplyr::data_frame(
+    coef =
+      base::c(
+        "(Intercept)",
+        base::attr(x, "x")
+      ),
+    "Coefficient value" =
+      base::colMeans(samples),
+    ci =
+      base::apply(samples, 2, sd) %>%
+      magrittr::multiply_by(1.96)
+  ) %>%
+  coef_plot_
+}
+
+#' @describeIn coef_plot coefficient plot for rjags and rjags.parallel objects
+setMethod(
+  "coef_plot",
+  base::c(x = "rjags"),
+  coef_plot_rjags
+)
+
+#' @describeIn coef_plot coefficient plot for rjags and rjags.parallel objects
+setMethod(
+  "coef_plot",
+  base::c(x = "rjags.parallel"),
+  coef_plot_rjags
+)
+
+utils::globalVariables(base::c("Coefficient value", "ci"))
